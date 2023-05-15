2bf76105441c13c45b29620732f73963c16e6a4f
keeganpearson <keegan.dpearson@gmail.com>
2023-05-15 22:45:26 +0000

    Changes keys input from console logs to actual movements on server side and adds TCP connection

--- input.js
+++ input.js
@@ -1,16 +1,43 @@
+// store active TCP connection object
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  // assig passed connection object to out scope 'connection' variable
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
   const handleUserInput = function (key) {
+    // Ctrl C, exit the process
     if (key === '\u0003') {
-      // check for Ctrl + C input
       process.exit();
     }
+
+    // w = "Move: up"
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+
+    // a = "Move: left"
+    if (key === 'a') {
+      connection.write("Move: left");
+    }
+
+    // s = "Move: down"
+    if (key === 's') {
+      connection.write("Move: down");
+    }
+
+    // d = "Move: right"
+    if (key === 'd') {
+      connection.write("Move: right");
+    }
   };
+
   // register event listener
   stdin.on("data", handleUserInput);
 
