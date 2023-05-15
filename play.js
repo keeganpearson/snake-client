02b3375718830bb678c4367c5e182452f28279f5
keeganpearson <keegan.dpearson@gmail.com>
2023-05-15 22:08:15 +0000

    moves setUpInput and handleUserInput to separate file input.js

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 const net = require("net");
 const connect = require("./client");
+const setupInput = require("./input");
 
 // create the connection object
 const conn = connect();
@@ -12,25 +13,6 @@ conn.on("data", (data) => {
 });
 
 console.log("Connecting ...");
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      // check for Ctrl + C input
-      process.exit();
-    }
-  };
-  // register event listener
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
 
 // call 
 setupInput();
