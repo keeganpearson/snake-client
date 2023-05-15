e1db04ca1033ba2bf889c77018fbb25661d7820e
keeganpearson <keegan.dpearson@gmail.com>
2023-05-15 22:00:41 +0000

    Adds ability to listen for keyboard input, and implements listening for Ctrl C to stop game running

--- play.js
+++ play.js
@@ -12,3 +12,25 @@ conn.on("data", (data) => {
 });
 
 console.log("Connecting ...");
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      // check for Ctrl + C input
+      process.exit();
+    }
+  };
+  // register event listener
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+// call 
+setupInput();
